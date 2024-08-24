417b00ec63258558868bfeb6039a58ff3b36237b
Fingertips <ghiantan0418@gmail.com>
2024-08-24 18:12:38 +0800

    Create folder structure and change assets

--- app/index.jsx
+++ app/index.jsx
@@ -2,13 +2,15 @@ import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 import { Link } from "expo-router";
 
+import { Routes } from "../constants/routes";
+
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-2xl font-poppins-black">Vexa</Text>
       <StatusBar style="auto" />
-      <Link href={"/profile"} className="text-blue-400 font-poppins-medium">
-        Go to Profile
+      <Link href={Routes.home} className="text-blue-400 font-poppins-medium">
+        Go to Home
       </Link>
     </View>
   );
